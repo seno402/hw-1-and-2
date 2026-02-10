90ece396ced868d7f24109ea58185f35379225f7
seno402 <reallbob2020@gmail.com>
2026-02-10 19:05:15 +0000

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number"))
+
+count = 0
+
+if num == 0:
+    count = 1
+else:
+    if num < 0:
+        num = -num
+
+    while num > 0:
+        count += 1
+        num //= 10
+
+print("total digits", count)
